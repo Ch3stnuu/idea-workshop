336dc50b16856ebc4ab97620f2d29b4538f9fd21
heeeyou <741234559@qq.com>
2018-07-12 16:06:23 +0800

    init

---
+++ src/main/java/cn/chestnut/ideaworkshop/common/MyConfig.java
@@ -0,0 +1,43 @@
+package cn.chestnut.ideaworkshop.common;
+
+
+import com.jfinal.config.*;
+import com.jfinal.kit.PropKit;
+import com.jfinal.template.Engine;
+
+/**
+ * @author 74123
+ */
+public class MyConfig extends JFinalConfig {
+    @Override
+    public void configConstant(Constants me) {
+        me.setUrlParaSeparator("&");
+        PropKit.use("config.properties");
+        me.setDevMode(true);
+    }
+
+    @Override
+    public void configRoute(Routes me) {
+
+    }
+
+    @Override
+    public void configEngine(Engine me) {
+
+    }
+
+    @Override
+    public void configPlugin(Plugins me) {
+
+    }
+
+    @Override
+    public void configInterceptor(Interceptors me) {
+
+    }
+
+    @Override
+    public void configHandler(Handlers me) {
+
+    }
+}
